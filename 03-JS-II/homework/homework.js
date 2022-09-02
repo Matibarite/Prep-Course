c1ae9bf744fafd279a856b47d516c4f32d2e8ac0
Matias Barite <matybarite@gmail.com>
2022-09-02 19:54:34 -0300

    modified homework 25

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -192,6 +192,11 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
+var numero = 6;
+
+for(i = 0; i < 10; i++){
+  return 6 * i;
+}
 }
 
 function tieneTresDigitos(numero){
