7e7d2608b98b0ce0720e137b287b34e459319941
Matias Barite <matybarite@gmail.com>
2022-09-01 21:47:51 -0300

    modified homework 18

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -241,21 +241,34 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var stringExclamacion = str + '!';
+  return stringExclamacion;
+
 }
+
+agregarSimboloExclamacion('Hola');
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  var nombreCompleto = nombre + ' ' + apellido;
+  return nombreCompleto;
   
 }
+
+combinarNombres('Matias', 'Barite');
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  var saludo = 'Hola ' + nombre + '!';
+  return saludo;
   
 }
+
+obtenerSaludo('Mati');
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
