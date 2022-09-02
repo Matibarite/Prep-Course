d7a1cd09bfc966f478c63b424570caf25dfb945a
Matias Barite <matybarite@gmail.com>
2022-09-01 22:16:07 -0300

    modified homework 21

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -317,8 +317,8 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-
-  let vocales = [a, e, i, o, u]
+  let vocales = ['a', 'e', 'i', 'o', 'u']
+  
   if(letra.length === 1){
   } else if(letra = vocales) {
     return 'Es vocal';
@@ -326,7 +326,7 @@ function esVocal(letra){
 
 }
 
-esVocal(a);
+esVocal('a');
 
 // No modificar nada debajo de esta línea
 // --------------------------------
