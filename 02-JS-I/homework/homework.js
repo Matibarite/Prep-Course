9a0400b7235459e94b22d0a084a26a2f62661992
Matias Barite <matybarite@gmail.com>
2022-09-01 23:15:10 -0300

    modified homework 30

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -319,7 +319,7 @@ function esVocal(letra){
   //Escribe tu código aquí
 
 
-  if(letra.length === 1 && letra === 'a' && letra === 'e' && letra === 'i' && letra === 'o' && letra === 'u'){
+  if(letra.length === 1 || letra === 'a' && letra === 'e' && letra === 'i' && letra === 'o' && letra === 'u'){
     return 'Es vocal';
   } else {
     return 'Dato incorrecto';
