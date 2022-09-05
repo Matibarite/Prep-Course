72334ecc57a02f119dd1ab59dc658dbf2610a858
Matias Barite <matybarite@gmail.com>
2022-09-05 13:22:22 -0300

    Modified homework 5/9

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -203,9 +203,9 @@ function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
   if(numero.length === 3){
-    return true;
-  } else{
     return false;
+  } else{
+    return true;
   }
 }
 
